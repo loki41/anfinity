38db7ac8182dc59e9d9e25b54f1446b88a44ebca
loki41 <amcl86@gmail.com>
2013-12-19 20:29:22 +0000

    system constants and updated sitemap
    Moved todo form choices to constants file and updated sitemap

---
+++ config/initializers/anfinity_constants.rb
@@ -0,0 +1,3 @@
+LOCATIONS = ["Under the hood", "New Section", "Portfolio", "About", "Blog", "Cook Book", "Contact"]
+TOPIC_KINDS = ["New Feature", "New Section", "Technical Update", "Bug", "New Content", "Design Update", "Redesign"]
+STATUS = ["Pending", "In Progress", "Complete", "Cancelled"]
