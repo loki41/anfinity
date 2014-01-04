bb2f239a4f13949e6dd86395be9c14d98818fccd
loki41 <amcl86@gmail.com>
2014-01-04 16:41:27 +0000

    Posts controller for draft functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,25 +8,15 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    if params[:month]
-      @posts = filter_month.to_a.paginate(page: params[:page], per_page: 5)
-    elsif params[:year]
-	  @posts = filter_year.to_a.paginate(page: params[:page], per_page: 5)
-	elsif params[:cat]
-	  filter_cats
-	elsif params[:tag]
-	  @posts = Post.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-    else
-      @posts = Post.all.paginate(page: params[:page], per_page: 5)
-    end
+   
+   index_filter
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
     set_post
-    
-	  add_breadcrumb @post.title, @post
+    add_breadcrumb @post.title, @post
   end
 
   # GET /posts/new
@@ -79,23 +69,23 @@ class PostsController < ApplicationController
   end
   
   def filter_month
-    @posts.delete_if { |y| y.created_at.strftime("%Y") != params[:year] }.delete_if { |m| m.created_at.strftime("%m") != params[:month] }
+    publish_filter.delete_if { |y| y.created_at.strftime("%Y") != params[:year] }.delete_if { |m| m.created_at.strftime("%m") != params[:month] }
   end
   
   def filter_year
-    @posts.delete_if { |y| y.created_at.strftime("%Y") != params[:year] }
+    publish_filter.delete_if { |y| y.created_at.strftime("%Y") != params[:year] }
   end
   
   def filter_cats
-	  @posts.delete_if { |c| c.category.to_i != params[:cat].to_i }
+	publish_filter.delete_if { |c| c.category.to_i != params[:cat].to_i }
   end
   
   def recent
-    @recent = Post.limit(5)
+    @recent = publish_filter.limit(5)
   end
   
   def archive
-    @posts = Post.all
+    @posts = publish_filter
     @archive = Hash[ @posts.group_by{|y| y.created_at.year}.map{|k,v| [k,v.group_by{|m| m.created_at.strftime("%B")}]} ]
   end
   
@@ -103,14 +93,33 @@ class PostsController < ApplicationController
     @cats = Category.all
   end
   
+  def index_filter
+    if params[:month]
+      @posts = filter_month.to_a.paginate(page: params[:page], per_page: 5)
+    elsif params[:year]
+	  @posts = filter_year.to_a.paginate(page: params[:page], per_page: 5)
+	elsif params[:cat]
+	  filter_cats
+	elsif params[:tag]
+	  @posts = publish_filter.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+    else
+      @posts = publish_filter.all.paginate(page: params[:page], per_page: 5)
+    end
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.friendly.find(params[:id])
     end
+	
+	
+	def publish_filter
+	  user_signed_in? ? Post.all.paginate(page: params[:page], per_page: 5) : Post.where(status: 'Published').paginate(page: params[:page], per_page: 5)
+	end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :category, :tag_list, :comment, :created_at, :meta_title, :meta_description, :meta_keywords, :created_at, :topic)
+      params.require(:post).permit(:title, :content, :category, :tag_list, :comment, :created_at, :meta_title, :meta_description, :meta_keywords, :created_at, :topic, :status)
     end
 end
