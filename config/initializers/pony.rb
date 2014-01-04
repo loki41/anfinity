1ebccafda1c5d28678f802b1ac2499d90e714d07
loki41 <amcl86@gmail.com>
2014-01-04 14:49:30 +0000

    responsive video player

---
+++ config/initializers/pony.rb
@@ -0,0 +1,13 @@
+Pony.options = {
+  :to => ENV['MY_GMAIL'],
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => ENV['MY_GMAIL'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+}
