e985caf34fd36c00b73deebbbb00dbbfc23609c0
anthony mclaughlin <anthony.mclaughlin@examtime.com>
2015-05-10 22:51:44 +0100

    updated show migration

--- db/migrate/20150510212619_add_show_to_projects.rb
+++ db/migrate/20150510212619_add_show_to_projects.rb
@@ -1,5 +1,5 @@
 class AddShowToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :show, :boolean, default: 1
+    add_column :projects, :show, :boolean, default: true
   end
 end
